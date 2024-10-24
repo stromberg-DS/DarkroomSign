8f9b02770cb0d96dba2bbf4de0c99f121f9b4d4b
DStromberg <strombergundy@gmail.com>
2024-10-24 10:52:15 -0600

    LEDs working w/switch

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ const int PIXEL_COUNT = 36;
 
 const int LED_PIN = 6;
 const int ENTER_PIN = 9;
-const int NO_ENTTER_PIN = 8;
+const int NO_ENTER_PIN = 8;
 const int POT_PIN = A3;
 const int PARTY_PIN = 5;
 const int POT_HYSTERSIS = 10;
@@ -20,7 +20,7 @@ Adafruit_NeoPixel pixel(PIXEL_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   Serial.begin(9600);
   pinMode(ENTER_PIN, INPUT_PULLUP);
-  pinMode(NO_ENTTER_PIN, INPUT_PULLUP);
+  pinMode(NO_ENTER_PIN, INPUT_PULLUP);
   pinMode(PARTY_PIN, INPUT_PULLUP);
   pinMode(POT_PIN, INPUT);
 
@@ -42,8 +42,23 @@ void setup() {
 }
 
 void loop() {
+  bool isEnterOn = digitalRead(ENTER_PIN);
+  bool isNoEnterOn = digitalRead(NO_ENTER_PIN);
   brightness = analogRead(POT_PIN);
   brightness = map(brightness, 0, 1024, 0, 255);
+
+  if(!isEnterOn){
+    pixel.clear();
+    for(int i=0; i<COLOR_CHANGE_LED; i++){
+      pixel.setPixelColor(i, 0x00AA00);
+    }
+  } else if(!isNoEnterOn){
+    for(int i=0; i<PIXEL_COUNT; i++){
+      pixel.setPixelColor(i, 0xAA0000);
+    }
+  } else{
+    pixel.clear();
+  }
   
   if(abs(lastBrightness-brightness)>POT_HYSTERSIS){
     pixel.setBrightness(brightness);

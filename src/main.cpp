13473351937ebe288f24e786e6a4f75f4c9a3736
DStromberg <strombergundy@gmail.com>
2024-10-25 09:25:54 -0600

    Setup switch input as button objects, updated LED numbers

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Adafruit_NeoPixel.h>
 #include<Button.h>
 
-const int PIXEL_COUNT = 36;
+const int PIXEL_COUNT = 475;
 
 const int LED_PIN = 6;
 const int ENTER_PIN = 9;
@@ -11,7 +11,7 @@ const int POT_PIN = A3;
 const int PARTY_PIN = 5;
 const int POT_HYSTERSIS = 10;
 
-const int COLOR_CHANGE_LED = 10;   //The LED number where the optional "Do not" text starts
+const int COLOR_CHANGE_LED = 350;   //The LED number where the optional "Do not" text starts
 
 int brightness = 100;
 int lastBrightness;
@@ -24,6 +24,8 @@ unsigned long lastPrintTime = 0;
 
 Adafruit_NeoPixel pixel(PIXEL_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 Button partyButton(PARTY_PIN);
+Button enterButton(ENTER_PIN);
+Button noEnterButton(NO_ENTER_PIN);
 
 void segmentBreathe(int startLED, int endLED, uint32_t breatheColor, float speed, int amplitude, int yOffset);
 void intervalPrint();
@@ -40,11 +42,11 @@ void setup() {
   pixel.show();
   pixel.setBrightness(brightness);
   
-  for(int i=0; i<PIXEL_COUNT; i++){
-    pixel.setPixelColor(i, 0x0000AA);
-    pixel.show();
-    delay(50);
-  }
+  //  LED TEST
+  // for(int i=0; i<PIXEL_COUNT; i++){
+  //   pixel.setPixelColor(i, 0x0000AA);
+  //   pixel.show();
+  // }
   delay(2000);
 
   // pixel.clear();
@@ -52,24 +54,27 @@ void setup() {
 }
 
 void loop() {
-  bool isEnterOn = digitalRead(ENTER_PIN);
-  bool isNoEnterOn = digitalRead(NO_ENTER_PIN);
   baselineBrightness = analogRead(POT_PIN);
+  int enterBrightness = map(baselineBrightness, 0, 1024, 0, 255);
   baselineBrightness = map(baselineBrightness, 0, 1024, 60, 195);
 
   if(partyButton.isClicked()){
     isPartytime = !isPartytime;
+    pixel.clear();
   }
 
-  intervalPrint();
+  // intervalPrint();
 
   if(isPartytime){
-    segmentBreathe(5, 10, 0x00FFFF, 0.25, 60, baselineBrightness);
+    segmentBreathe(0, PIXEL_COUNT, 0x00FFFF, 0.25, 60, baselineBrightness);
   }else{
-    if(!isEnterOn){
-      segmentBreathe(0, COLOR_CHANGE_LED, 0x00FF00, 2.0, 60, baselineBrightness);
-    } else if(!isNoEnterOn){
-      segmentBreathe(0, PIXEL_COUNT, 0xFF0000, 0.5, 60, baselineBrightness);
+    if(enterButton.isPressed()){
+      pixel.setBrightness(enterBrightness);
+      for(int i=0; i<COLOR_CHANGE_LED; i++){
+        pixel.setPixelColor(i, 0x00FF00);
+      }
+    } else if(noEnterButton.isPressed()){
+      segmentBreathe(0, PIXEL_COUNT, 0xFF0000, 0.25, 60, baselineBrightness);
 
     } else{
       pixel.clear();

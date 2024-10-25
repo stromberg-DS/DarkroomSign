efcd756bc929494d9e4aae2d1a6ef0ea978c360e
DStromberg <strombergundy@gmail.com>
2024-10-25 10:21:00 -0600

    Fine tuned party mode, renamed file.

--- src/DarkroomSign.cpp
+++ src/DarkroomSign.cpp
@@ -20,6 +20,11 @@ float breatheBrightness;
 int baselineBrightness = 127;
 bool isPartytime = false;
 
+int pixelDelay = 50;
+int pixelCycle = 0;
+unsigned long pixelPrevious = 0;
+int rainbowSpeed = 5;
+
 unsigned long lastPrintTime = 0;
 
 Adafruit_NeoPixel pixel(PIXEL_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -29,6 +34,8 @@ Button noEnterButton(NO_ENTER_PIN);
 
 void segmentBreathe(int startLED, int endLED, uint32_t breatheColor, float speed, int amplitude, int yOffset);
 void intervalPrint();
+uint32_t wheel(byte wheelPos);
+void rainbow(uint8_t wait);
 
 void setup() {
   Serial.begin(9600);
@@ -47,7 +54,7 @@ void setup() {
   //   pixel.setPixelColor(i, 0x0000AA);
   //   pixel.show();
   // }
-  delay(2000);
+  // delay(2000);
 
   // pixel.clear();
   pixel.show();
@@ -55,18 +62,30 @@ void setup() {
 
 void loop() {
   baselineBrightness = analogRead(POT_PIN);
-  int enterBrightness = map(baselineBrightness, 0, 1024, 0, 255);
-  baselineBrightness = map(baselineBrightness, 0, 1024, 60, 195);
+  int enterBrightness = map(baselineBrightness, 0, 1024, 255, 0);
+  rainbowSpeed = map(baselineBrightness, 0, 1024, 35, 1);
+  baselineBrightness = map(baselineBrightness, 0, 1024, 195, 60);
 
   if(partyButton.isClicked()){
     isPartytime = !isPartytime;
     pixel.clear();
   }
 
+  if(enterButton.isReleased() || enterButton.isClicked()) {
+    pixel.clear();
+    isPartytime=false;
+  }
+  if(noEnterButton.isReleased() || noEnterButton.isClicked()) isPartytime=false;
+
   // intervalPrint();
 
   if(isPartytime){
-    segmentBreathe(0, PIXEL_COUNT, 0x00FFFF, 0.25, 60, baselineBrightness);
+    // segmentBreathe(0, PIXEL_COUNT, 0x00FFFF, 0.25, 60, baselineBrightness);
+
+    if(millis() - pixelPrevious >=pixelDelay){
+      pixelPrevious = millis();
+      rainbow(1);
+    }
   }else{
     if(enterButton.isPressed()){
       pixel.setBrightness(enterBrightness);
@@ -109,4 +128,34 @@ void intervalPrint(){
     Serial.println(baselineBrightness);
     lastPrintTime = millis();
   }
+}
+
+void rainbow(uint8_t wait){
+    if(pixelDelay != wait){
+        pixelDelay = wait;
+    }
+
+    for(uint16_t i=0; i<PIXEL_COUNT; i++){
+        pixel.setPixelColor(i, wheel((i+pixelCycle) &255));
+    }
+    pixel.show();
+    pixelCycle +=rainbowSpeed;
+    // pixelCycle++;
+    if(pixelCycle >= 256){
+        pixelCycle =0;
+    }
+}
+
+uint32_t wheel(byte wheelPos){
+    wheelPos = 255 - wheelPos;
+    if(wheelPos < 85){
+        return pixel.Color(255-wheelPos*3, 0, wheelPos * 3);
+    }
+
+    if(wheelPos < 170){
+        wheelPos -= 85;
+        return pixel.Color(0, wheelPos*3, 255-wheelPos*3);
+    }
+    wheelPos -=170;
+    return pixel.Color(wheelPos*3, 255-wheelPos*3, 0);
 }
